7e54d7e540079139fe333ba923b2f3b8b61c5b8f
pablocorrea14 <pcorrib@g.educaand.es>
2025-04-07 13:32:15 +0200

    camvbio 3

--- Proyecto 1 Entorno/src/paquete1/Principal.java
+++ Proyecto 1 Entorno/src/paquete1/Principal.java
@@ -3,7 +3,7 @@ package paquete1;
 public class Principal {
 
   public static void main(String[] args) {
-   System.out.print("Estamos haciendo control de versiones con Git 2");
+   System.out.print("Estamos haciendo control de versiones con Git 2 papa");
 
   }
 
